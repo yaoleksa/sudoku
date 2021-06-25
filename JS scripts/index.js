7f63f27bc40a71dbcb25b7da6c8ee74582f520ac
PavloMartovichPowerSnow <mryaremchyk@gmail.com>
2021-06-25 12:57:54 +0300

    Add validate algorithm

--- JS scripts/index.js
+++ JS scripts/index.js
@@ -1,8 +1,8 @@
+const validateTable = require('./validator.js');
+
 const main=document.getElementById('main');
 
 const table=[1, 2, 3, 4, 5, 6, 7, 8, 9];
-const verticalValues=[];
-const horizontalValues=[];
 
 for(let i=0; i<9; i++) {
     let nested=document.createElement('div');
@@ -26,15 +26,13 @@ for(let i=0; i<9;i++) {
     for(let j=0;j<9;j++) {
         let nestedDiv=document.createElement('div');
         nestedDiv.setAttribute('class', 'inner_square');
-        /*let coincidence=parseInt(Math.random()*2);
+        let coincidence=parseInt(Math.random()*2);
         let buf=table[parseInt(Math.random() * table.length)];
-        if(coincidence===1&&!verticalValues.includes(buf)&&!horizontalValues.includes(buf)) {
+        if(coincidence===1&&validateTable.validate(validateTable.horizontalValues, validateTable.verticalValues, count, buf)) {
             let p=document.createElement('p');
             p.innerHTML = buf;
             nestedDiv.appendChild(p);
             table.splice(table.indexOf(buf), 1);
-            verticalValues.push(buf);
-            horizontalValues.push(buf);
         } else {
             let input=document.createElement('input');
             input.setAttribute('class', 'num_inp');
@@ -42,11 +40,8 @@ for(let i=0; i<9;i++) {
             input.setAttribute('min', '1');
             input.setAttribute('max', '9');
             nestedDiv.appendChild(input);
-        }*/
-        let p = document.createElement('p');
-        p.innerHTML = count;
+        }
         count++;
-        nestedDiv.appendChild(p);
         div.appendChild(nestedDiv);
     }
     
@@ -57,6 +52,5 @@ for(let i=0; i<9;i++) {
     if((i+1)%3===0) {
         horizontalValues.length=0;
     }
-    // TODO: create algorithm to reset verticalValues
     divWithTable.appendChild(div);
 }
